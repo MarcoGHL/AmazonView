e8d6a4c89652f96eedbf244f0ab34d38f748eaf7
MarcoGHL <marco.guhl38@gmail@gmail.com>
2021-05-29 00:33:06 +0200

    send discord messages

--- Amazon.py
+++ Amazon.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import unicodedata
 
-url = "https://www.amazon.de/Sony-Interactive-Entertainment-PlayStation-5/dp/B08H93ZRK9/ref=sr_1_2?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=ps5&qid=1622147092&sr=8-2"
-HEADERS = ({'User-Agent':"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"})
+url = "https://www.amazon.de/AmazonBasics-hy3-Festplattentasche-schwarz/dp/B003LSTD38?ref_=ast_sto_dp&th=1&psc=1"
+HEADERS = { "user-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
 
 def get_product_info(url):
     page = requests.get(url, headers=HEADERS)
@@ -15,5 +15,26 @@ def get_product_info(url):
     
     except:
         print('failed')
+
+    try:
+        soup.select('#availability .a-color-success')[0].get_text().strip()
+        available = True
+        print('product available')
+
+        payload = {
+            'content' : 'Produkt: ' + title + ' ist wieder unter ' + url + ' verfügbar'
+        }
+
+        headers = {
+            'authorization' : 'NzAyMTMzMDMyODE5MjI4NzMy.YFoGqQ.6Gph8fuNtUQTHJeYuY4VGYe_fio'
+        }
+        r = requests.post("https://discordapp.com/api/v9/channels/847958015067029547/messages", data = payload, headers=headers)
+
+    except:
+        available = False
+        print('product not available')
+
+    return title, available
+
 
 get_product_info(url)
